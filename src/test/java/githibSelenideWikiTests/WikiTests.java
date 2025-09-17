98389ead0aea70e140529b9b0f1e42ce42feff87
roman.anisimov <roman.anisimov@simbirsoft.com>
2025-09-17 16:17:04 +0300

    Wiki test with text

---
+++ src/test/java/githibSelenideWikiTests/WikiTests.java
@@ -0,0 +1,31 @@
+package githibSelenideWikiTests;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class WikiTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";   // нужный размер
+        // или
+        // Configuration.startMaximized = true;     // работает только в 6.x и ниже
+    }
+
+    @Test
+    public void wikiTest(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $$("ul a.internal.present").shouldHave(CollectionCondition.itemWithText("Soft assertions"));
+        $(withText("Soft assertions")).click();
+        Configuration.timeout = 5000;
+        $("#wiki-body").shouldHave(text("JUnit5 extension"));
+    }
+}
